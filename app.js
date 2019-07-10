e087e779f305a96ba8f3a733812d3eb640037d03
Ashell Fernando <ashellz2010@gmail.com>
2019-07-10 12:17:26 -0400

    added validation, listPlayers, whosAlive

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let inquirer = require('inquirer');
 
 let playersArr = [];
 
+//character object constructor fx
 function Character(name, profession, gender, age, strength, hp) {
   this.name = name;
   this.profession = profession;
@@ -52,6 +53,7 @@ function Character(name, profession, gender, age, strength, hp) {
 // Morty.levelUp();
 // Morty.printStats();
 
+//character creation
 function promptCreateChar() {
   inquirer
     .prompt([
@@ -73,17 +75,41 @@ function promptCreateChar() {
       {
         type: 'input',
         name: 'age',
-        message: "What's the Character's age?"
+        message: "What's the Character's age?",
+        validate: function(input) {
+          if (parseInt(input)) {
+            return true;
+          } else {
+            console.log('\nType in  an integer\n');
+            return false;
+          }
+        }
       },
       {
         type: 'input',
         name: 'strength',
-        message: "What's the Character's strength?"
+        message: "What's the Character's strength?",
+        validate: function(input) {
+          if (parseInt(input) || parseFloat(input)) {
+            return true;
+          } else {
+            console.log('\nType in  an integer\n');
+            return false;
+          }
+        }
       },
       {
         type: 'input',
         name: 'hp',
-        message: "What's the Character's hp?"
+        message: "What's the Character's hp?",
+        validate: function(input) {
+          if (parseInt(input) || parseFloat(input)) {
+            return true;
+          } else {
+            console.log('\nType in  an integer\n');
+            return false;
+          }
+        }
       }
     ])
     .then(answers => {
@@ -91,15 +117,41 @@ function promptCreateChar() {
         answers.name,
         answers.profession,
         answers.gender,
-        answers.age,
-        answers.strength,
-        answers.hp
+        parseInt(answers.age),
+        parseFloat(answers.strength),
+        parseFloat(answers.hp)
       );
       playersArr.push(char);
-      console.log(playersArr);
       init();
     });
 }
+
+//finds out who is alive currently
+function whoIsAlive() {
+  if (playersArr.length <= 0) {
+    console.log('No players currently');
+    init();
+  } else {
+    for (let key in playersArr) {
+      if (playersArr[key].hp > 0) {
+        console.log(playersArr[key].name);
+      }
+    }
+  }
+}
+
+function listPlayers() {
+  console.log(
+    `Found ${playersArr.length} player${playersArr.length > 1 ? 's' : ''}`
+  );
+  for (let key in playersArr) {
+    let player = playersArr[key];
+    console.log(`${JSON.stringify(player, null, 2)}`);
+  }
+  init();
+}
+
+//main menu of the game
 function init() {
   inquirer
     .prompt([
@@ -123,12 +175,15 @@ function init() {
           promptCreateChar();
           break;
         case 'List Players':
-          if (playersArr.length > 1) {
-            JSON.stringify(playersArr, null, 2);
+          if (playersArr.length >= 1) {
+            listPlayers();
           } else {
             console.log("There aren't any current players");
             init();
           }
+          break;
+        case 'Who is alive?':
+          whoIsAlive();
           break;
         case 'Exit':
           console.log(`Okay Byee!!`);
