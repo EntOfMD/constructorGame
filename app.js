52303e5f1c804de3facd9b57ef6d044d2e78ca0c
Ashell Fernando <ashellz2010@gmail.com>
2019-07-13 23:26:59 -0400

    +xp, +level, +faker pkg

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let inquirer = require('inquirer');
+let faker = require('faker');
 
 let playersArr = [];
 
@@ -10,15 +11,19 @@ function Character(name, profession, gender, age, strength, hp) {
   this.age = age;
   this.strength = strength;
   this.hp = hp;
+  this.xp = 0;
+  this.level = 1;
   this.printStats = function() {
     console.log(`
-      STATS FOR THE CHARACTER:
+      STATS FOR THE ${this.name.toUpperCase()}:
         Name: ${this.name},
         Profession: ${this.profession},
         Gender: ${this.gender},
         Age: ${this.age},
         Strength: ${this.strength},
-        HitPoints: ${this.hp}
+        HitPoints: ${this.hp},
+        XP: ${this.xp},
+        Level: ${this.level}
         `);
   };
   this.isAlive = function() {
@@ -27,32 +32,48 @@ function Character(name, profession, gender, age, strength, hp) {
     );
   };
   this.Attack = function(opponent) {
+    this.xp = opponent.strength * 0.1 + this.strength;
     opponent.hp -= this.strength;
+    this.strength = opponent.strength * 0.25 + this.strength;
+    if (this.xp >= 100) {
+      this.levelUp();
+    }
     console.log(
       `${this.name} attacked ${opponent.name} with ${this.strength} dmg`
     );
+    if (opponent.hp <= 0) {
+      opponent.isAlive();
+    }
   };
   this.levelUp = function() {
+    this.xp = 0;
     this.age++;
     this.strength += 5;
     this.hp += 25;
+    this.level += 1;
   };
 }
 
-let Rick = new Character('Rick', 'Scientist', 'Alien', 100, 100, 999);
+let Rick = new Character('Rick', 'Scientist', 'Male', 100, 100, 999);
 
-let Morty = new Character('Morty', 'morty', 'Male', 20, 10, 100);
+let Morty = new Character('Morty', 'Subject', 'Male', 20, 10, 100);
 playersArr.push(Rick, Morty);
 
-// Rick.printStats();
-// Morty.printStats();
-
-// Morty.Attack(Rick);
-// Rick.printStats();
-// Rick.isAlive();
-
-// Morty.levelUp();
-// Morty.printStats();
+//seed
+function seedChars() {
+  for (let i = 0; i < 20; i++) {
+    let newChar = new Character(
+      faker.name.firstName(),
+      faker.name.jobTitle(),
+      faker.name.suffix(),
+      Math.floor(Math.random() * 100),
+      Math.floor(Math.random() * 100),
+      Math.floor(Math.random() * 100)
+    );
+    playersArr.push(newChar);
+  }
+  console.log('Added 20 new random players');
+}
 
 //character creation
 function promptCreateChar() {
@@ -148,7 +169,7 @@ function listPlayers() {
   );
   for (let key in playersArr) {
     let player = playersArr[key];
-    console.log(`${JSON.stringify(player, null, 2)}`);
+    console.log(`${player.name}`);
   }
   init();
 }
@@ -163,7 +184,9 @@ function attackPlayer() {
         choices: function() {
           let players = [];
           playersArr.forEach(i => {
-            players.push(i.name);
+            if (i.hp > 0) {
+              players.push(i.name);
+            }
           });
           return players;
         }
@@ -176,10 +199,25 @@ function attackPlayer() {
             type: 'list',
             message: `Who is ${answer.attacker} attacking?`,
             name: 'foe',
+            validate: function() {
+              for (let key in playersArr) {
+                if (
+                  playersArr[key].name !== answer.attacker &&
+                  playersArr[key].hp > 0
+                ) {
+                  return true;
+                } else {
+                  return false;
+                }
+              }
+            },
             choices: function() {
               let foes = [];
               for (let key in playersArr) {
-                if (playersArr[key].name !== answer.attacker) {
+                if (
+                  playersArr[key].name !== answer.attacker &&
+                  playersArr[key].hp > 0
+                ) {
                   foes.push(playersArr[key].name);
                 }
               }
@@ -190,6 +228,8 @@ function attackPlayer() {
         .then(newAnswer => {
           let attackerObj = {};
           let foeObj = {};
+
+          //Finds if an object exist
           for (let key in playersArr) {
             if (
               playersArr[key].name === answer.attacker &&
@@ -197,18 +237,30 @@ function attackPlayer() {
             ) {
               attackerObj = playersArr[key];
               // console.log('Attacker = ' + JSON.stringify(attackerObj, null, 2));
-              console.log(attackerObj);
+              // console.log(attackerObj);
             } else if (
               playersArr[key].name === newAnswer.foe &&
               playersArr[key] instanceof Object
             ) {
               foeObj = playersArr[key];
               // console.log('Foe = ' + JSON.stringify(foeObj, null, 2));
-              console.log(foeObj);
+              // console.log(foeObj);
             }
           }
 
-          attackerObj.Attack(foeObj.name);
+          //prints stats
+          attackerObj.printStats();
+          foeObj.printStats();
+
+          //attacks
+          if (foeObj.hp > 0) {
+            attackerObj.Attack(foeObj);
+          } else {
+            foeObj.isAlive();
+          }
+          // attackerObj.printStats();
+          // foeObj.printStats();
+          init();
         });
     });
 }
@@ -219,20 +271,20 @@ function init() {
     .prompt([
       {
         type: 'list',
-        name: 'init',
+        name: 'mainMenu',
         choices: [
           'Create Character',
           'List Players',
           'Who is alive?',
           'Attack',
-          'Level Up',
           'Print Stats',
+          'Seed Players',
           'Exit'
         ]
       }
     ])
     .then(answer => {
-      switch (answer.init) {
+      switch (answer.mainMenu) {
         case 'Create Character':
           promptCreateChar();
           break;
@@ -250,6 +302,20 @@ function init() {
 
         case 'Attack':
           attackPlayer();
+          break;
+
+        case 'Print Stats':
+          if (playersArr.length >= 1) {
+            for (key in playersArr) {
+              playersArr[key].printStats();
+              console.log('***************');
+            }
+          }
+          init();
+          break;
+        case 'Seed Players':
+          seedChars();
+          init();
           break;
         case 'Exit':
           console.log(`Okay Byee!!`);
